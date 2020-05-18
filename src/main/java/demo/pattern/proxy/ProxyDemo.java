fd56b586b37209b889a875867f52260f34a73af0
Gepeng18 <robin_ge@foxmail.com>
2020-05-18 23:24:10 +0800

    提出方法

--- src/main/java/demo/pattern/proxy/ProxyDemo.java
+++ src/main/java/demo/pattern/proxy/ProxyDemo.java
@@ -11,26 +11,50 @@ import java.lang.reflect.InvocationHandler;
 
 public class ProxyDemo {
     public static void main(String[] args) {
-//        ToCPayment toCProxy = new AlipayToC(new ToCPaymentImpl());
-//        toCProxy.pay();
-//        ToBPayment toBProxy = new AlipayToB(new ToBPaymentImpl());
-//        toBProxy.pay();
-//        ToCPayment toCPayment = new ToCPaymentImpl();
-//        InvocationHandler handler = new AlipayInvocationHandler(toCPayment);
-//        ToCPayment toCProxy = JdkDynamicProxyUtil.newProxyInstance(toCPayment,handler);
-//        toCProxy.pay();
-//        ToBPayment toBPayment = new ToBPaymentImpl();
-//        InvocationHandler handlerToB = new AlipayInvocationHandler(toBPayment);
-//        ToBPayment toBProxy = JdkDynamicProxyUtil.newProxyInstance(toBPayment, handlerToB);
-//        toBProxy.pay();
+        proxyPattern();
+
+        trueJdkProxy();
+
+        falseJdkProxy();
+
+        trueCglibProxy();
+    }
+
+    private static void proxyPattern() {
+        ToCPayment toCProxy = new AlipayToC(new ToCPaymentImpl());
+        toCProxy.pay();
+        ToBPayment toBProxy = new AlipayToB(new ToBPaymentImpl());
+        toBProxy.pay();
+    }
+
+    private static void trueJdkProxy() {
+        ToCPayment toCPayment = new ToCPaymentImpl();
+        InvocationHandler handler = new AlipayInvocationHandler(toCPayment);
+        ToCPayment toCProxy = JdkDynamicProxyUtil.newProxyInstance(toCPayment,handler);
+        toCProxy.pay();
+        ToBPayment toBPayment = new ToBPaymentImpl();
+        InvocationHandler handlerToB = new AlipayInvocationHandler(toBPayment);
+        ToBPayment toBProxy = JdkDynamicProxyUtil.newProxyInstance(toBPayment, handlerToB);
+        toBProxy.pay();
+    }
+
+    private static void falseJdkProxy() {
         CommonPayment commonPayment = new CommonPayment();
-//        AlipayInvocationHandler invocationHandler = new AlipayInvocationHandler(commonPayment);
-//        CommonPayment commonPaymentProxy = JdkDynamicProxyUtil.newProxyInstance(commonPayment, invocationHandler);
+        AlipayInvocationHandler invocationHandler = new AlipayInvocationHandler(commonPayment);
+        CommonPayment commonPaymentProxy = JdkDynamicProxyUtil.newProxyInstance(commonPayment, invocationHandler);
+        commonPaymentProxy.pay();
+    }
+
+
+    private static void trueCglibProxy() {
+        CommonPayment commonPayment = new CommonPayment();
         MethodInterceptor methodInterceptor = new AlipayMethodInterceptor();
         CommonPayment commonPaymentProxy = CglibUtil.createProxy(commonPayment, methodInterceptor);
         commonPaymentProxy.pay();
+
         ToCPayment toCPayment = new ToCPaymentImpl();
         ToCPayment toCProxy = CglibUtil.createProxy(toCPayment, methodInterceptor);
         toCProxy.pay();
     }
+
 }
